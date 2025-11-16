c1ddc59d7666312d350f7c7fd160d543f9f74caa
Nantapop Chonchobthum <nantapop.cho@dome.tu.ac.th>
2025-11-17 01:28:35 +0700

    add return Id

--- src/main/java/com/example/demo/repository/FormRepository.java
+++ src/main/java/com/example/demo/repository/FormRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.Form;
 
 public interface FormRepository extends JpaRepository<Form, Long> {
+    
+    
+    Form findByPetId(String petId);
 
 }
